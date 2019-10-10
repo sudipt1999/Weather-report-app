17808b98438dd51b0e276584b0c40fc5968d027f
Manuel <jose.manuel.oviedo.guzman@gmail.com>
2019-10-10 12:58:20 -0500

    Changed modal content and data structure

---
+++ utils/headers.js
@@ -0,0 +1,199 @@
+const headers = {
+    "time": {
+        "text": "Time",
+        "show": true,
+        "default": true
+    },
+    "summary": {
+        "text": "Summary",
+        "show": true,
+        "default": true
+    },
+    "icon": {
+        "text": "https://darksky.net/images/weather-icons/",
+        "show": false,
+        "default": false
+    },
+    "sunriseTime": {
+        "text": "Sunrise",
+        "show": true,
+        "default": true
+    },
+    "sunsetTime": {
+        "text": "Sunset",
+        "show": true,
+        "default": true
+    },
+    "moonPhase": {
+        "text": "Moon phase",
+        "show": true,
+        "default": false
+    },
+    "precipIntensity": {
+        "text": "Precipitation intensity",
+        "show": true,
+        "default": false
+    },
+    "precipIntensityMax": {
+        "text": "Max precipitation intensity",
+        "show": true,
+        "default": false
+    },
+    "precipIntensityMaxTime": {
+        "text": "Time max precipitation intensity",
+        "show": true,
+        "default": false
+    },
+    "precipProbability": {
+        "text": "Precipitation probability",
+        "show": true,
+        "default": false
+    },
+    "precipType": {
+        "text": "Precipitation type",
+        "show": true,
+        "default": false
+    },
+    "temperatureHigh": {
+        "text": "Highest",
+        "show": true,
+        "default": true
+    },
+    "temperatureHighTime": {
+        "text": "Highest time",
+        "show": true,
+        "default": false
+    },
+    "temperatureLow": {
+        "text": "Lowest",
+        "show": true,
+        "default": true
+    },
+    "temperatureLowTime": {
+        "text": "Lowest time",
+        "show": true,
+        "default": false
+    },
+    "apparentTemperatureHigh": {
+        "text": "Apparent highest",
+        "show": true,
+        "default": false
+    },
+    "apparentTemperatureHighTime": {
+        "text": "Apparent highest time",
+        "show": true,
+        "default": false
+    },
+    "apparentTemperatureLow": {
+        "text": "Apparent lowest",
+        "show": true,
+        "default": false
+    },
+    "apparentTemperatureLowTime": {
+        "text": "Apparent lowest time",
+        "show": true,
+        "default": false
+    },
+    "dewPoint": {
+        "text": "Dew point",
+        "show": true,
+        "default": false
+    },
+    "humidity": {
+        "text": "Humidity",
+        "show": true,
+        "default": true
+    },
+    "pressure": {
+        "text": "Pressure",
+        "show": true,
+        "default": false
+    },
+    "windSpeed": {
+        "text": "Wind speed",
+        "show": true,
+        "default": true
+    },
+    "windGust": {
+        "text": "Wind gust",
+        "show": true,
+        "default": false
+    },
+    "windGustTime": {
+        "text": "Time wind gust",
+        "show": true,
+        "default": false
+    },
+    "windBearing": {
+        "text": "Wind bearing",
+        "show": true,
+        "default": false
+    },
+    "cloudCover": {
+        "text": "Cloud coverage",
+        "show": true,
+        "default": true
+    },
+    "uvIndex": {
+        "text": "UV index",
+        "show": true,
+        "default": true
+    },
+    "uvIndexTime": {
+        "text": "Time UV index",
+        "show": true,
+        "default": false
+    },
+    "visibility": {
+        "text": "Visibility",
+        "show": true,
+        "default": false
+    },
+    "ozone": {
+        "text": "Ozone",
+        "show": true,
+        "default": false
+    },
+    "temperatureMin": {
+        "text": "Min",
+        "show": true,
+        "default": true
+    },
+    "temperatureMinTime": {
+        "text": "Min time",
+        "show": true,
+        "default": false
+    },
+    "temperatureMax": {
+        "text": "Max",
+        "show": true,
+        "default": false
+    },
+    "temperatureMaxTime": {
+        "text": "Max time",
+        "show": true,
+        "default": false
+    },
+    "apparentTemperatureMin": {
+        "text": "Min feels like",
+        "show": true,
+        "default": true
+    },
+    "apparentTemperatureMinTime": {
+        "text": "Apparent min tempreature time",
+        "show": true,
+        "default": false
+    },
+    "apparentTemperatureMax": {
+        "text": "Max feels like",
+        "show": true,
+        "default": true
+    },
+    "apparentTemperatureMaxTime": {
+        "text": "Apparent max temperature time",
+        "show": true,
+        "default": false
+    }
+};
+
+module.exports = headers;
