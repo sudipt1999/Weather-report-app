e548f1600e606dafef08dc07ab5c4f266f9dfc87
Taran Vohra <taran.vohra@hotmail.com>
2019-10-10 13:09:40 +0530

    Checkboxes in Weather Report

--- views/main.js
+++ views/main.js
@@ -1,88 +1,102 @@
 $(function() {
-    
-    const btn = $('#search-btn')
-    const address = $('#place-name')
+  const btn = $('#search-btn');
+  const address = $('#place-name');
+  let checkboxStates = {};
+  btn.click(function(event) {
+    submitAddress(address.val());
+    address.val('');
+  });
 
+  address.keypress(function(event) {
+    //check if enter key is pressed
+    if (event.which == 13) {
+      submitAddress(address.val());
+      address.val('');
+    }
+  });
 
-    btn.click(function(event) {
-        submitAddress(address.val())
-        address.val("")
-    })
+  const showModal = data => {
+    if (!data) {
+      return;
+    }
 
-    address.keypress(function(event) {
-        //check if enter key is pressed
-        if (event.which == 13){
-            submitAddress(address.val())
-            address.val("") 
-        }
-     });
+    render(data);
+    $('.modal').modal('show');
+  };
 
+  const render = data => {
+    const forecastDataKeys = Object.keys(data.forecastData);
+    const place = $('#place');
+    place.text(data.location);
 
-     const showModal = (data) => {
-         if(!data){
-             return;
-         }
+    const checkboxes = $('#checkboxes');
+    const checkboxesValues = forecastDataKeys.map(key => {
+      checkboxStates[key] = true;
+      return `<div><input type="checkbox" id="${key}" checked><label for="${key}">${key}</label></div>`;
+    });
 
+    checkboxes.append(checkboxesValues.join(' '));
 
-         const place = $('#place')
-         place.text(data.location)
+    forecastDataKeys.map(key => {
+      $(`#${key}`).click(function() {
+        checkboxStates[key] = !checkboxStates[key];
+        console.log(checkboxStates);
+        renderTable(data);
+      });
+    });
 
-         const tbody = $('#tbody')
-         let values = Object.keys(data.forecastData).map(key=>{
-            return(
-                `<tr>
+    renderTable(data);
+  };
+
+  const renderTable = data => {
+    const forecastDataKeys = Object.keys(data.forecastData);
+
+    const tbody = $('#tbody');
+    tbody.empty();
+    let values = forecastDataKeys.map(key => {
+      if (!checkboxStates[key]) return ``;
+      return `<tr>
                     <td>${key}</td>
                     <td>${data.forecastData[key]}</td>
                 </tr>
-                `
-            )
-         })
+                `;
+    });
+    tbody.append(values.join());
+  };
 
-         tbody.append(values.join())
-         $('.modal').modal('show')
-     }
+  const submitAddress = address => {
+    if (address.length == 0) {
+      // a invalid address showing a model stating invlaid address
+      $('#invalid-alert').addClass('show');
+      setTimeout(() => {
+        $('#invalid-alert').removeClass('show');
+      }, 2000);
+      return;
+    }
 
-
-
-
-
-     const submitAddress = (address) => {
-         if(address.length == 0) {
-             // a invalid address showing a model stating invlaid address
-             $('#invalid-alert').addClass('show')
-             setTimeout(()=>{
-                $('#invalid-alert').removeClass('show')
-             },2000)
-             return
-            }
-
-            // making call to server for weather report
-            const data = JSON.stringify({address})
-            console.log(data)
-            $.ajax({
-
-                url : '/',
-                type : 'POST',
-                data : data,
-                dataType:'json',
-                contentType: 'application/json',
-                success : function(data) {              
-                  //  alert('Data:');
-                    console.log(data)
-                    showModal(data)
-                },
-                error : function(request,error)
-                {
-                    console.log(error)
-                    // a invalid address showing a model stating invlaid address
-                    $('#invalid-alert').addClass('show')
-                    setTimeout(()=>{
-                        $('#invalid-alert').removeClass('show')
-                    },2000)
-                    return
-                }
-            });
-
-     }
-
+    // making call to server for weather report
+    const data = JSON.stringify({ address });
+    console.log(data);
+    $.ajax({
+      url: '/',
+      type: 'POST',
+      data: data,
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function(data) {
+        //  alert('Data:');
+        console.log(data);
+        showModal(data);
+      },
+      error: function(request, error) {
+        console.log(error);
+        // a invalid address showing a model stating invlaid address
+        $('#invalid-alert').addClass('show');
+        setTimeout(() => {
+          $('#invalid-alert').removeClass('show');
+        }, 2000);
+        return;
+      }
+    });
+  };
 });
